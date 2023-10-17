d2e85b127e87aa843305a586955d786a3a6966c5
Mamba <zukiswamamba@gmail.com>
2023-10-17 15:51:07 +0200

    JS
    Updated date, temperature, humidity,wind and description

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
-  let temElement = document.querySelector("#current-temp");
+  let tempElement = document.querySelector("#current-temp");
   let cityElement = document.querySelector("#current-city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
+  let dateElement = document.querySelector("#current-date");
   tempElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 
 let apiKey = "a4dbff3fb9894f32e6a77bt933fco6a0";
